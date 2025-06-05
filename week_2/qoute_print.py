1eab346f4f2f6b4a57d1bde1f5acf5df41fde58d
John <john.kuronya@outlook.com>
2025-06-05 09:31:35 -0400

    Second Commit

---
+++ week_2/qoute_print.py
@@ -0,0 +1,11 @@
+"""
+Author: John Kuronya
+Date: June 2, 2025
+File Name: quote_print.py
+Description: This file demonstrates the use inline comments.
+It prints a quote and gives credit to the original author.
+"""
+
+# Printing a quote and crediting the author
+print('"The only thing we have to fear is fear itself."')
+print("- Franklin D. Roosevelt")
